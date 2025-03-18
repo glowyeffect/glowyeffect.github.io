50226817d34196138cd1129e67ad4d4ae09dfb37
Debbie Yuen <deborahyuen2016@berkeley.edu>
2025-03-17 20:15:41 -0700

    add topic folders

--- glowyeffect-app/app/layout.config.tsx
+++ glowyeffect-app/app/layout.config.tsx
@@ -10,7 +10,8 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
-      <>
+      
+      <> 
         <svg
           width="24"
           height="24"
@@ -19,14 +20,27 @@ export const baseOptions: BaseLayoutProps = {
         >
           <circle cx={12} cy={12} r={12} fill="currentColor" />
         </svg>
-        My App
+        {/* <span className= "bg-green-300 border-gray-500 dark: text-black"> 
+          Glowy Effect International
+        </span> */}
+        Glowy International
       </>
     ),
   },
   links: [
     {
-      text: 'Documentation',
+      text: 'Mission',
+      url: '/mission',
+      active: 'nested-url',
+    },
+    {
+      text: 'Learn',
       url: '/docs',
+      active: 'nested-url',
+    },
+    {
+      text: 'Blog',
+      url: '/blog',
       active: 'nested-url',
     },
   ],
